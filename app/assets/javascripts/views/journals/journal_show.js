e8c120ec740c3d7612a5c182b431397b8d13d10b
CrookedKings <John.Mahowald@gmail.com>
2014-09-25 18:01:19 -0700

    adding css transitions

--- app/assets/javascripts/views/journals/journal_show.js
+++ app/assets/javascripts/views/journals/journal_show.js
@@ -9,6 +9,12 @@ AnnotateLife.Views.JournalShow = Backbone.CompositeView.extend({
     this.listenTo(this.chapters, "storiesReady", this.attachStoriesIndex);
   },
   
+  events: {
+    "click #move-button": "promptChapterSelect",
+    "click #move-back": "promptStorySelect",
+    "click #edit-the-story": "storyEditMode"
+  },
+  
   attributes: { class: 'journal-show-container'},
   
   template: JST["journals/show"],
@@ -43,5 +49,26 @@ AnnotateLife.Views.JournalShow = Backbone.CompositeView.extend({
   attachStoryEditForm: function() {
     var storyEditForm = new AnnotateLife.Views.StoryForm();
     this.addSubview(".story-edit", storyEditForm);
+  },
+  
+  promptChapterSelect: function() {
+    $('.stories').addClass('hidden');
+    $('.story-edit').addClass('hidden');
+    $('.chapters').addClass('col-xs-offset-4');
+  },
+  
+  promptStorySelect: function() {
+    $('.chapters').removeClass('col-xs-offset-4 col-xs-4');
+    $('.chapters').addClass('col-xs-2');
+    $('.stories').removeClass('col-xs-4 hidden');
+    $('.stories').addClass('col-xs-5 col-xs-offset-1');
+  },
+  
+  storyEditMode: function() {
+    $('.stories').removeClass('col-xs-5 col-xs-offset-1');
+    $('.stories').addClass('col-xs-3')
+    $('.story-edit').removeClass('hidden')
+    $('.story-edit').removeClass('col-xs-4 hidden');    
+    $('.story-edit').addClass('col-xs-7');
   }
 });
